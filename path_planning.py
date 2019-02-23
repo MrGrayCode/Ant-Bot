b52f8a1edf9715fc6c107765aaa97502ec2f95fa
Akshatha nayak <akshanayaki@gmail.com>
2019-02-23 19:29:24 +0530

    minor fixes

--- path_planning.py
+++ path_planning.py
@@ -13,6 +13,13 @@ graph = AStarGraph()
 src = (7,3) #initialize location
 current_direction = "U"	#directed upwards on the grid
 
+getLoc = {  '00': {'sr1': (1,11),  'sr2': (3,11), 'ah': (2,11)},
+            '01': {'sr1': (11,11), 'sr2': (13,11), 'ah': (12,11)},
+            '10': {'sr1': (13,5),  'sr2': (11,5), 'ah': (12,5)},
+            '11': {'sr1': (3,5),   'sr2': (1,5), 'ah': (2,5)} }
+getDir = {  '0': {'sr1': "L",  'sr2': "R", 'ah': "U"},
+            '1': {'sr1': "R",   'sr2': "L", 'ah': "D"} }
+
 def queen(ids):
     for id in ids:
         if id[0]=='1':
@@ -27,13 +34,13 @@ def getMovesTo(dest, final_direction):
     src = dest
     return directions
 
-def service(ids, getSupply, trash_locations):
+def service(ids, getSupply):
     path_actions = []
     #Service queen anthill if it exists
     qid = queen(ids)
     if qid:
         #process trash service requirements
-        trash_service(qid, trash_locations, path_actions)
+        trash_service(qid, path_actions)
         #process SR2 for supply requirements
         supply_service(qid, qid[3:5], 'sr2', getSupply, path_actions)
         #process SR1 for supply requirements
@@ -43,7 +50,7 @@ def service(ids, getSupply, trash_locations):
     for id in ids:
         if id != qid:
             #process trash service requirements
-            trash_service(id, trash_locations, path_actions)
+            trash_service(id, path_actions)
             #process SR2 for supply requirements
             supply_service(id, id[3:5], 'sr2', getSupply, path_actions)
             #process SR1 for supply requirements
@@ -52,7 +59,6 @@ def service(ids, getSupply, trash_locations):
     #return to start position and turn on buzzer
     path_actions += getMovesTo((7,1), "D")
     path_actions.append("BUZZER")
-
     return path_actions
 
 def supply_service(id, sr, sr_num, getSupply, path_actions):
@@ -76,62 +82,32 @@ def supply_service(id, sr, sr_num, getSupply, path_actions):
         path_actions += getMovesTo(getLoc[id[1:3]]['ah'], dir)
         path_actions.append("DEPOSIT SUPPLY")
 
-def trash_service(id, trash_locations, path_actions):
+def trash_service(id, path_actions):
     global current_direction
     #if AH has trash service requirement
     if id[7] != '0':
         ah_loc = getLoc[id[1:3]]['ah']
         dir = getDir[id[1]]['ah']
-        path_actions += getMovesTo(ah_loc, dir)
-        #Case#1: (trash, supply) -> if SR2 has supply requirement, SR1 has trash
-        if id[3:5] != '00':
-            dir = getDir[id[1]]['sr1']
-            trash_loc = getLoc[id[1:3]]['sr1']
-        #if SR2 has no supply requirement
+        path_actions += getMovesTo(ah_loc, dir) #now facing the wall
+        #perform trash pick up routine
+        path_actions.append("SCAN & PICK TRASH")
+        if dir=="U":
+            current_direction = "D"
         else:
-            #Case#2: (supply, trash) -> if SR1 has supply requirement, SR2 has trash
-            if id[5:7] != '00':
-                dir = getDir[id[1]]['sr2']
-                trash_loc = getLoc[id[1:3]]['sr2']
-            #Case#3 & #4 (no service, trash) & (trash, no service)
-            else:
-                #scan for trash on left (sr1)
-                path_actions.append("SCAN TRASH")
-                #if trash found in SR1
-                if getLoc[id[1:3]]['sr1'] in trash_locations:
-                    dir = getDir[id[1]]['sr1']
-                    trash_loc = getLoc[id[1:3]]['sr1']
-                #if trash not found in SR1, it is in SR2
-                else:
-                    dir = getDir[id[1]]['sr2']
-                    trash_loc = getLoc[id[1:3]]['sr2']
-
-        #go to the service region in AH requiring trash service and pick trash
-        path_actions += getMovesTo(ah_loc, dir)
-        path_actions.append("PICK TRASH")
-        trash_locations.remove(trash_loc)
-
+            current_direction = "U"
+        #now facing the anthill exit
         #go to trash deposit zone and deposit trash
         path_actions += getMovesTo((7,10), "U")  #coordinates of Trash Deposit Zone
         path_actions.append("DEPOSIT TRASH")
 
-#get Aruco IDs in binary format
-getLoc = {  '00': {'sr1': (1,11),  'sr2': (3,11), 'ah': (2,11)},
-            '01': {'sr1': (11,11), 'sr2': (13,11), 'ah': (12,11)},
-            '10': {'sr1': (13,5),  'sr2': (11,5), 'ah': (12,5)},
-            '11': {'sr1': (3,5),   'sr2': (1,5), 'ah': (2,5)} }
-getDir = {  '0': {'sr1': "L",  'sr2': "R", 'ah': "U"},
-            '1': {'sr1': "R",   'sr2': "L", 'ah': "D"} }
-
+###########################DELETE THIS###############################
 #####################SAMPLE CONFIGURATION###################
 ids = ['00100001','01011010','11101001','00000100'] #example
 getSupply = {'01': [(3,3), (9,3)],  #red -> Honey Dew
              '10': [(5,3)],         #green -> Leaves
              '11': [(1,3), (11,3)]} #blue -> Wood
-trash_locations = [(13,11), (3,5)]
 #############################################################
 
-##################To main.py#################################
 #scan all supplies in the shrub region
 path_actions = []
 #go to each supply location in left shrub region and scan it (camera on left, so no turns)
@@ -148,10 +124,4 @@ path_actions += getMovesTo((7,8),"U") #add the coordinates of central node
 path_actions.append("READ ARUCO")
 
 #do all servicing, return to start, buzzer on
-path_actions2 = service(ids, getSupply, trash_locations)
-
-#display actions
-for i in path_actions + path_actions2:
-    print(i, end=", ")
-    if len(i)>1:
-        print("")
+print(service(ids, getSupply))
